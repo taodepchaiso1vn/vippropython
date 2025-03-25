f7c94e3f9d4ce05296a7fa35af25993c287c6b8d
taodepchaiso1vn <khaihienpubg@gmail.com>
2025-03-25 23:12:15 +0700

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+# vippropython'
+x = input("Nhập số nguyên:")
+x = int(x)
+if x>0:
+    print(x, "là số dương")
+if x%2==0:
+    print(x,"la so chan")
+else:
+    print(x,"la so le")
+
+print("xuat ket qua:", x,"la so chan")
